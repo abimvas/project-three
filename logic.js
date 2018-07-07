1c6e2c67173a743ec9c41ec7400c610db6e6bdd5
Jasmine Lo <jasmineelizalo@gmail.com>
2018-07-07 13:47:55 -0500

    more updates on logic.js

--- logic.js
+++ logic.js
@@ -1,70 +1,89 @@
-// var weather = final.csv
-// var hurricane = convertcsv-small.geojson
+var weatherData = "final.csv"
+var small = "convertercsv-small.geojson"
+
 
 // get data
-d3.csv("final.csv", function(data) {
+d3.csv(weatherData, function (weather) {
     //createFeatures(data.features);
-    console.log(data[0])
+    console.log(weather[0])
 });
 
-// define map
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
 
-function createMap(layers) {
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
 
-    // Define map layers
-    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
+
+// Define base maps
+var baseMaps = {
+    "Light Map": lightMap,
+    "Outdoors": outdoors,
+    "Satellite": satellite
+}
+
+// var baseLayer = L.tileLayer(
+//     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+//       attribution: '...',
+//       maxZoom: 18
+//     }
+//   );
+
+//   var cfg = {
+//     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+//     // if scaleRadius is false it will be the constant radius used in pixels
+//     "radius": 2,
+//     "maxOpacity": .8, 
+//     // scales the radius based on map zoom
+//     "scaleRadius": true, 
+//     // if set to false the heatmap uses the global maximum for colorization
+//     // if activated: uses the data maximum within the current map boundaries 
+//     //   (there will always be a red spot with useLocalExtremas true)
+//     "useLocalExtrema": true,
+//     // which field name in your data represents the latitude - default "lat"
+//     latField: 'lat',
+//     // which field name in your data represents the longitude - default "lng"
+//     lngField: 'lng',
+//     // which field name in your data represents the data value - default "value"
+//     valueField: 'count'
+//   };
   
-    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
+//   var heatmapLayer = new HeatmapOverlay(cfg);
 
-    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVsaXphbG8iLCJhIjoiY2pneWI3b2szMDF6YzMzbXoxMGFpdnlzdSJ9.eFiZjkE9_VHob0oOHTauSQ");
-    
-      // Define base maps
-    var baseMaps = {
-        "Light Map": lightMap,
-        "Outdoors": outdoors,
-        "Satellite": satellite
   
-    }};
+//   heatmapLayer.setData(weather);
 
-    // map =
+var map = L.map("map", {
+    center: [29.365721, -35.004062],
+    zoom: 2,
+    layers: Object.values(baseMaps)
+});
+satellite.addTo(map)
 
-    // map.addSource('hurricane', {
-    //     type: 'geojson',
-    //     data: 'csvconvert1.geojson'
-    // });
-    // map.addLayer({
-    //     id: 'hurricane',
-    //     source: 'hurricane',
-    //     type: 'heatmap'
-    // });
 
-    // var hurricane = new L.LayerGroup();
+//  //Add layer control to map
+// L.control.layers(baseMaps, [outdoors], {
+//     collapsed: false
+// }).addTo(map);
 
-    // var overlayMaps = {
-    //     "Temperature": data,
-    //     "Hurricane": hurricane
-    // };
 
-    var map = L.map("map", {
-        center: [40.7128, -74.0059],
-        zoom: 11
-      });
+// //Create a marker layer (in the example done via a GeoJSON FeatureCollection)
+// var testlayer = L.geoJson(json);
+// var sliderControl = L.control.sliderControl({position: "topright", layer: testlayer});
 
-    function gatherDataPoints(hurricane){
+// //Make sure to add the slider to the map ;-)
+// map.addControl(sliderControl);
 
-        L.geoJson(hurricane).addTo(map);
-      };
-    
-      $.getJSON("convertcsv-small.geojson", gatherDataPoints);
-     
-     // $.getJSON("convertcsv2.geojson", gatherDataPoints);
+// //And initialize the slider
+// sliderControl.startSlider();
 
-    //  //Add layer control to map
-    //  L.control.layers(baseMaps, overlayMaps, {
-    //     collapsed: false
-    // }).addTo(myMap);
+// var testlayer = L.geoJson(json);
+// var sliderControl = L.control.sliderControl({position: "topright", layer: testlayer});
 
+// //Make sure to add the slider to the map ;-)
+// map.addControl(sliderControl);
+
+// //And initialize the slider
+// sliderControl.startSlider();
 
     //  legend.addTo(myMap);
 
-    
